1cb9247880a7eb7600f786b96ba77df81ca487bc
Adri Saha <adrisaha53@gmail.com>
2026-04-12 16:55:08 +0530

    Final Code

--- add_numbers.c
+++ add_numbers.c
@@ -2,10 +2,11 @@
 int main ()
 {
 	int a,b,sum;
-	printf("Enter first number-\n");
-	printf("Enter second number-\n");
-	scanf("%d %d",&a,&b);
+	printf("Enter first number- \n");
+	scanf("%d",&a);
+	printf("Enter second number- \n");
+	scanf("%d",&b);
 	sum = a + b;
-	printf("the sum is-%d\n",sum);
+	printf("the sum is- %d\n",sum);
 	return 0;
 }
