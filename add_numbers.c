5204b9dca8c6899e6eeff41760684982fd78cd2a
Adri Saha <252322517+adriBeach7@users.noreply.github.com.>
2026-04-24 20:49:30 +0530

    Fixing accidental Removal of File

---
+++ add_numbers.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main ()
+{
+	int a,b,sum;
+	printf("Enter first number- \n");
+	scanf("%d",&a);
+	printf("Enter second number- \n");
+	scanf("%d",&b);
+	sum = a + b;
+	printf("the sum is- %d\n",sum);
+	return 0;
+}
